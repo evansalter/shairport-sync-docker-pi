29ee3618f9ab2daf7bffe5d862cafd5c4666e8c0
Evan Salter <evanearlsalter@gmail.com>
2017-05-15 23:17:35 -0600

    Remove commented out code

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-#systemctl start dbus
-#RUN /etc/init.d/dbus start
-#RUN /etc/init.d/avahi-daemon start
-#systemctl enable avahi-daemon
-#systemctl start avahi-daemon
-
 service dbus start
 service avahi-daemon start
 
